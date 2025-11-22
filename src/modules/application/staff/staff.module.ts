bc92c5711fb5d212a5fa069d943bad863447c0d1
Nirob844 <nirobhossainalip@gmail.com>
2025-11-22 12:22:12 +0600

    staff home page

--- src/modules/application/staff/staff.module.ts
+++ src/modules/application/staff/staff.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ApplyShiftModule } from './apply-shift/apply-shift.module';
 import { GeofenceModule } from './geofence/geofence.module';
 import { ProfileModule } from './profile/profile.module';
+import { HomeModule } from './home/home.module';
 
 
 @Module({
-    imports: [ApplyShiftModule, GeofenceModule, ProfileModule],
+    imports: [ApplyShiftModule, GeofenceModule, ProfileModule, HomeModule],
 })
 export class StaffModule { }
